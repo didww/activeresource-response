f00c9d9ccc006bdca9b37e4441512c43dc6c870f
Igor F <fedoronchuk@gmail.com>
2012-02-15 21:00:32 +0200

    can be called once in base class inherited from  ActiveResource

--- lib/activeresource-response.rb
+++ lib/activeresource-response.rb
@@ -1,28 +1,30 @@
 require "activeresource-response/version"
 module ActiveresourceResponse
+   module Connection
+     def self.included(base)
+       base.class_eval  <<-EOS
+           alias_method :origin_handle_response, :handle_response 
+           attr_reader :http_response
+           def handle_response(response)
+             @http_response= response
+             origin_handle_response(response)
+           end
+        EOS
+     end 
+   end   
    module AddResponseMethod
      def self.included(base)
         base.extend ClassMethods
       end
       module ClassMethods
-         def add_response_method(method_name = 'response')
-            
-            connection.class_eval  <<-EOS
-                alias_method :origin_handle_response, :handle_response 
-                attr_reader :#{method_name}
-                def handle_response(response)
-                  @#{method_name}= response
-                  origin_handle_response(response)
-                end
-             EOS
-          
+         def add_response_method(method_name = 'http_response')
              class_eval  <<-EOS
                class << self
                  alias_method :origin_find, :find
                  def find(*arguments)
                      result = origin_find(*arguments)
                      result.class_eval("attr_reader :#{method_name}")
-                     result.instance_variable_set(:"@#{method_name}", connection.#{method_name})
+                     result.instance_variable_set(:"@#{method_name}", connection.http_response)
                      result
                  end
                 end 
@@ -31,5 +33,5 @@ module ActiveresourceResponse
       end
    end   
 end
-
+ActiveResource::Connection.send :include, ActiveresourceResponse::Connection
 ActiveResource::Base.send :include, ActiveresourceResponse::AddResponseMethod
