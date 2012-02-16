4b2a0d2059e73ac1a419c9e0ef8a0e76e921a8d1
Igor F <fedoronchuk@gmail.com>
2012-02-16 12:37:57 +0200

    http response now stored in Thread.current

--- lib/activeresource-response.rb
+++ lib/activeresource-response.rb
@@ -2,14 +2,16 @@ require "activeresource-response/version"
 module ActiveresourceResponse
    module Connection
      def self.included(base)
-       base.class_eval  <<-EOS
+       base.class_eval  do
            alias_method :origin_handle_response, :handle_response 
-           attr_reader :http_response
            def handle_response(response)
-             @http_response= response
+             Thread.current['ActiveResource::Base.http_response'] = response
              origin_handle_response(response)
            end
-        EOS
+           def http_response
+              Thread.current['ActiveResource::Base.http_response']
+           end    
+        end
      end 
    end   
    module AddResponseMethod
