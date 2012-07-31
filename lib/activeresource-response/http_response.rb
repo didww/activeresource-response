5c7222691bf0bf42a622c21e7af529f33ee47961
Igor <fedoronchuk@gmail.com>
2012-07-31 16:54:31 +0300

    warning 'instance variable @_active_resource_response_* not initialized' fixed

--- lib/activeresource-response/http_response.rb
+++ lib/activeresource-response/http_response.rb
@@ -2,11 +2,14 @@ module ActiveresourceResponse
   module HttpResponse
     
     def headers
-      @_active_resource_response_headers || symbolize_keys(to_hash)
+      unless defined? @_active_resource_response_headers
+        @_active_resource_response_headers = symbolize_keys(to_hash)
+      end
+      @_active_resource_response_headers
     end
          
     def cookies
-      unless @_active_resource_response_cookies
+      unless defined? @_active_resource_response_cookies
         @_active_resource_response_cookies    = (self.headers[:set_cookie] || {}).inject({}) do |out, cookie_str|
           CGI::Cookie::parse(cookie_str).each do |key, cookie|
             out[key] = cookie.value.first unless ['expires', 'path'].include? key
