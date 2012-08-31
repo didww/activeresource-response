5e4bd7b35200d2c4e80c32cb7f887272943c7e4f
Igor <fedoronchuk@gmail.com>
2012-09-01 02:32:31 +0300

    licence comments

--- lib/active_resource_response/response.rb
+++ lib/active_resource_response/response.rb
@@ -1,15 +1,15 @@
-  module ActiveResourceResponse
-    module Response
-      def self.included(base)
-        base.class_eval  do
-          def to_hash
-            @headers
-          end
-          # to avoid method name conflict with Response:HttpResponse:headers
-          def [](key)
-            @headers[key]
-          end
+module ActiveResourceResponse
+  module Response
+    def self.included(base)
+      base.class_eval  do
+        def to_hash
+          @headers
+        end
+        # to avoid method name conflict with Response:HttpResponse:headers
+        def [](key)
+          @headers[key]
         end
       end
     end
   end
+end
