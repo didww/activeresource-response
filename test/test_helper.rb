e0f68061628165ef90fd13cd9aac419070cb50a7
Igor Fedoronchuk <igor.f@didww.com>
2014-07-24 00:20:17 +0300

    test_helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,18 +1,15 @@
-require 'minitest/autorun'
 require 'coveralls'
+Coveralls.wear!
+require 'minitest/autorun'
 require 'i18n'
 lib = File.expand_path("#{File.dirname(__FILE__)}/../lib")
 unit_tests = File.expand_path("#{File.dirname(__FILE__)}/../test")
 $:.unshift(lib)
 $:.unshift(unit_tests)
-
-
+I18n.config.enforce_available_locales = true
 require 'active_resource_response'
 require "fixtures/country"
 require "fixtures/city"
 require "fixtures/region"
 require "fixtures/street"
 require "active_resource_response/http_mock"
-
-I18n.config.enforce_available_locales = true
-Coveralls.wear!
